7defebc7e4a86df6b8848fd68d4c3bb8458c4c47
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-03-23 04:16:50 -0300

    feat: start student service to process resource

---
+++ back/Student/src/main/java/br/com/school/resources/services/StudentService.java
@@ -0,0 +1,13 @@
+package br.com.school.resources.services;
+
+import br.com.school.domains.repositories.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+
+    @Autowired
+    private StudentRepository repository;
+
+}
