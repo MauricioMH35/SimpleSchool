1d15f5901972420d2fef930535d3d5e2e56a0231
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-03-23 04:15:42 -0300

    feat: start contact repository interface

---
+++ back/Student/src/main/java/br/com/school/domains/repositories/ContactRepository.java
@@ -0,0 +1,11 @@
+package br.com.school.domains.repositories;
+
+import br.com.school.domains.entities.Contact;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, UUID> {
+}
