34e87c6cb9a0666c506f77f558fbba8eeb67cec2
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-03-23 04:14:43 -0300

    feat: start student repository interface

---
+++ back/Student/src/main/java/br/com/school/domains/repositories/StudentRepository.java
@@ -0,0 +1,12 @@
+package br.com.school.domains.repositories;
+
+import br.com.school.domains.entities.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, UUID> {
+
+}
