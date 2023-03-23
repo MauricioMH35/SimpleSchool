5cd0c4bcb893774b8e05515904280823f05e1a24
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-03-23 03:27:53 -0300

    feat: start student module

---
+++ back/Student/src/main/java/br/com/school/StudentApplication.java
@@ -0,0 +1,11 @@
+package br.com.school;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(StudentApplication.class, args);
+    }
+}
