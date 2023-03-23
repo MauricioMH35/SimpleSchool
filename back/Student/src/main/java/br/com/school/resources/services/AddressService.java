8b5007f8318a1fa6276a3bf2ca0d9571d7823ff3
Mauricio Mityo Hidani <mauriciomh35@gmail.com>
2023-03-23 04:17:30 -0300

    feat: start address service to process resource

---
+++ back/Student/src/main/java/br/com/school/resources/services/AddressService.java
@@ -0,0 +1,13 @@
+package br.com.school.resources.services;
+
+import br.com.school.domains.repositories.AddressRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressService {
+
+    @Autowired
+    private AddressRepository repository;
+
+}
